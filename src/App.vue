5cf1638ceacfe6f1ac6ee967811434dd3e5a157a
Nova <954669193@qq.com>
2022-08-16 16:23:10 +0800

    test: 增加 menu 测试相关

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,28 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
-import { MenuItemProgress } from '@mini-element-plus/components/menu'
-import { ref } from 'vue';
-const value = ref(0)
-const reference = ref()
-window.reference = reference
-window.setvalue = ()=> value.value+=1
+import { Menu, MenuInstance } from '@mini-element-plus/components/menu'
+import { ref } from 'vue'
+import { mebuData } from '../packages/components/menu/__test__/mock';
+mebuData
+import '@mini-element-plus/components/menu/style/index.scss'
+const reference = ref<MenuInstance>()
+const actions: any = {
+  ArrowDown: 'DOWN',
+  ArrowUp: 'UP',
+  ArrowLeft: 'LEFT',
+  ArrowRight: 'RIGHT',
+  Enter: 'ENTER',
+  Backspace: 'BACK',
+  Escape: 'BACK'
+}
+document.addEventListener('keyup', event => {
+  reference.value?.KEYDOWN(actions[event.key])
+})
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -19,11 +31,18 @@ window.setvalue = ()=> value.value+=1
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
-  <MenuItemProgress ref="reference" :min-size="0" :max-size="10" v-model="value"></MenuItemProgress>
+  <HelloWorld msg="Vite + Vue" /> -->
+  <Menu :data="mebuData" ref="reference" :visible="true">
+    <template #header>
+      <div style="font-size:.3rem;color:red">
+        i am menu title
+      </div>
+    </template>
+  </Menu>
 </template>
 
-<style scoped>
+<style>
+@import './assets/eng.css';
 .logo {
   height: 6em;
   padding: 1.5em;
